c3ca8af914de807e296b55e6f53167723bf3e46f
divesh1312 <diveshmaheshwari13@gmail.com>
2022-11-30 22:07:48 +0530

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Nav.css"
+function Navbar() {
+  return (
+    <div className="nav">
+       <img className='nav_logo'
+       src='https://1000logos.net/wp-content/uploads/2017/05/Netflix-Logo.png' 
+       alt='nav logo'
+       /> 
+    </div>
+  )
+}
+
+export default Navbar
